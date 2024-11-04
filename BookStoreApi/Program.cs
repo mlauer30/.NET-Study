4210053500d1bee54877b3cf4e6c291e163a8cc4
mlauer30 <mlauer07@gmail.com>
2024-11-04 18:13:22 -0500

    Working Mongodb Web Api, needed additions to program.cs

--- BookStoreApi/Program.cs
+++ BookStoreApi/Program.cs
@@ -2,10 +2,14 @@ using BookStoreApi.Models;
 using BookStoreApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.Configure<BookStoreDatabaseSettings>(
     builder.Configuration.GetSection("BookStoreDatabase"));
 
 builder.Services.AddSingleton<BooksService>();
+
+//not included in docs...
+builder.Services.AddControllers();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,6 +26,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// app.UseAuthorization();
+
+//not included in docs...
+app.MapControllers();
 
 var summaries = new[]
 {
